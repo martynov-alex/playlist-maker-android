4d150cba4db055c5882123abb4b7c17be90e6d07
Alex Martynov <martynov.alex@gmail.com>
2022-10-06 08:39:58 +0600

    add navigation

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,29 +15,21 @@ class MainActivity : AppCompatActivity() {
         val libraryButton = findViewById<Button>(R.id.library_button)
         val settingsButton = findViewById<Button>(R.id.settings_button)
 
-        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(
-                    this@MainActivity, "Нажали на кнопку поиска!", Toast.LENGTH_SHORT
-                ).show()
-            }
+        searchButton.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
         }
 
-        val libraryButtonClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(
-                    this@MainActivity, "Нажали на кнопку медиатеки!", Toast.LENGTH_SHORT
-                ).show()
-            }
+        libraryButton.setOnClickListener {
+            val displayIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(displayIntent)
         }
 
         settingsButton.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity, "Нажали на кнопку медиатеки!", Toast.LENGTH_SHORT
-            ).show()
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
         }
 
-        searchButton.setOnClickListener(searchButtonClickListener)
-        libraryButton.setOnClickListener(libraryButtonClickListener)
+
     }
 }
