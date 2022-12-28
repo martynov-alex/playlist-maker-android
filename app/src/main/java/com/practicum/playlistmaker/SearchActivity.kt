1b0a73d09d451d0decc1f0219035beecd4881e6e
Alex Martynov <martynov.alex@gmail.com>
2022-12-28 18:32:12 +0600

    edit after review

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -6,10 +6,8 @@ import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -37,39 +35,24 @@ class SearchActivity : AppCompatActivity() {
     // Search service init
     private val iTunesService = retrofit.create(ITunesApi::class.java)
 
-    private lateinit var searchResultRv: RecyclerView
-    private lateinit var nothingFoundPlaceholder: LinearLayout
-    private lateinit var errorPlaceholder: LinearLayout
+    private lateinit var inputEditText: EditText
+    private lateinit var clearButton: ImageView
     private lateinit var progressBar: ProgressBar
+    private lateinit var searchResultRv: RecyclerView
+    private lateinit var searchResultPlaceholder: LinearLayout
+    private lateinit var searchResultPlaceholderIcon: ImageView
+    private lateinit var searchResultPlaceholderText: TextView
+    private lateinit var searchResultPlaceholderButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val inputEditText = findViewById<EditText>(R.id.inputEditText)
-        val clearButton = findViewById<ImageView>(R.id.clearButton)
-        val repeatRequestButton = findViewById<Button>(R.id.repeat_request_button)
+        initViewVariables()
+        initListeners()
 
-        // Search results RecyclerView init
-        searchResultRv = findViewById(R.id.search_result_rv)
         searchResultRv.layoutManager = LinearLayoutManager(this)
         searchResultRv.adapter = SearchResultAdapter(tracks)
-
-        nothingFoundPlaceholder = findViewById(R.id.search_result_placeholder_nothing_found)
-        errorPlaceholder = findViewById(R.id.search_result_placeholder_error)
-        progressBar = findViewById(R.id.progressBar)
-
-        clearButton.setOnClickListener {
-            inputEditText.setText("")
-            inputEditText.hideKeyboard()
-            tracks.clear()
-            searchResultRv.adapter?.notifyDataSetChanged()
-        }
-
-        repeatRequestButton.setOnClickListener {
-            showResult(RequestResult.LOADING)
-            search(searchRequest)
-        }
 
         val searchFormTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -85,13 +68,6 @@ class SearchActivity : AppCompatActivity() {
         }
 
         inputEditText.addTextChangedListener(searchFormTextWatcher)
-        inputEditText.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                showResult(RequestResult.LOADING)
-                search(searchRequest)
-            }
-            false
-        }
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
@@ -116,13 +92,6 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
-    }
-
-    private fun View.hideKeyboard() {
-        val imm = ContextCompat.getSystemService(
-            context, InputMethodManager::class.java
-        ) as InputMethodManager
-        imm.hideSoftInputFromWindow(windowToken, 0)
     }
 
     private fun search(searchRequest: String) {
@@ -159,27 +128,64 @@ class SearchActivity : AppCompatActivity() {
             RequestResult.DONE -> {
                 searchResultRv.visibility = View.VISIBLE
                 progressBar.visibility = ProgressBar.GONE
-                nothingFoundPlaceholder.visibility = View.GONE
-                errorPlaceholder.visibility = View.GONE
+                searchResultPlaceholder.visibility = View.GONE
             }
             RequestResult.LOADING -> {
                 searchResultRv.visibility = View.GONE
                 progressBar.visibility = ProgressBar.VISIBLE
-                nothingFoundPlaceholder.visibility = View.GONE
-                errorPlaceholder.visibility = View.GONE
+                searchResultPlaceholder.visibility = View.GONE
             }
             RequestResult.NOTHING_FOUND -> {
+                searchResultPlaceholderIcon.setImageResource(R.drawable.search_result_placeholder_nothing_found_icon)
+                searchResultPlaceholderText.setText(R.string.search_result_placeholder_nothing_found)
+                searchResultPlaceholderButton.visibility = View.GONE
+
                 searchResultRv.visibility = View.GONE
                 progressBar.visibility = ProgressBar.GONE
-                nothingFoundPlaceholder.visibility = View.VISIBLE
-                errorPlaceholder.visibility = View.GONE
+                searchResultPlaceholder.visibility = View.VISIBLE
             }
             RequestResult.ERROR -> {
+                searchResultPlaceholderIcon.setImageResource(R.drawable.search_result_placeholder_error_icon)
+                searchResultPlaceholderText.setText(R.string.search_result_placeholder_error)
+                searchResultPlaceholderButton.visibility = View.VISIBLE
+
                 searchResultRv.visibility = View.GONE
                 progressBar.visibility = ProgressBar.GONE
-                nothingFoundPlaceholder.visibility = View.GONE
-                errorPlaceholder.visibility = View.VISIBLE
+                searchResultPlaceholder.visibility = View.VISIBLE
             }
+        }
+    }
+
+    private fun initViewVariables() {
+        inputEditText = findViewById(R.id.inputEditText)
+        clearButton = findViewById(R.id.clearButton)
+        progressBar = findViewById(R.id.progressBar)
+        searchResultRv = findViewById(R.id.search_result_rv)
+        searchResultPlaceholder = findViewById(R.id.search_result_placeholder)
+        searchResultPlaceholderIcon = findViewById(R.id.search_result_placeholder_icon)
+        searchResultPlaceholderText = findViewById(R.id.search_result_placeholder_text)
+        searchResultPlaceholderButton = findViewById(R.id.search_result_placeholder_button)
+    }
+
+    private fun initListeners() {
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            inputEditText.hideKeyboard()
+            tracks.clear()
+            searchResultRv.adapter?.notifyDataSetChanged()
+        }
+
+        searchResultPlaceholderButton.setOnClickListener {
+            showResult(RequestResult.LOADING)
+            search(searchRequest)
+        }
+
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                showResult(RequestResult.LOADING)
+                search(searchRequest)
+            }
+            false
         }
     }
 }
