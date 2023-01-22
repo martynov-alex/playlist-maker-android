1083ea19d56dd54037df37ae280269227f098c46
Alex Martynov <martynov.alex@gmail.com>
2023-01-22 18:09:58 +0600

    add audioplayer activity

---
+++ app/src/main/java/com/practicum/playlistmaker/activity/audioplayer/AudioPlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.practicum.playlistmaker.activity.audioplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.practicum.playlistmaker.R
+
+class AudioPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audioplayer)
+    }
+}
